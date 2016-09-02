0b366c53f4bc6a3e64fcbe4f8e3a4e502f442e2c
NazarRI <senior.ik@ya.ru>
2016-09-02 19:02:27 +0300

    Update Task2_4.java

--- src/Module2/Task2_4.java
+++ src/Module2/Task2_4.java
@@ -1,30 +1,24 @@
 package Module2;
 
 public class Task2_4 {
-    public static int[] balances = {1200, 100, 2000, 500, 3200};
+    private static int[] balances = {1200, 250, 2000, 500, 3200};
+    private static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
 
-    static double fundBalance(String[] ownerName, double fund) {
-        String ownN = ownerName[0];
-        double fun = fund;
-        double result;
-        double bal;
-        for (int i = 0; i < ownerName.length; i++) {
-            ownN = ownerName[3];
-        }
-        bal = balances[3];
-        result = bal + fun;
-        System.out.print(ownN + " ");
-
-        return result;
+    static double fundBalance(String ownername, double fund) {
+        double balance = 0;
+        for (int i = 0; i < ownerNames.length; i++) {
+           if(ownerNames[i] == ownername) {
+              balance = balances[i] + fund;
+           }
+       }
+        return balance;
     }
 
     public static void main(String[] args) {
+        String ownerName = "Ann";
+        double fund = 100;
 
-        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
-        int fund = 100;
-
-        double res = fundBalance(ownerNames, fund);
-        System.out.println(res);
-
+        double res = fundBalance(ownerName ,fund);
+        System.out.println(ownerName + " " + res);
     }
 }
