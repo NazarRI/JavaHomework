7e5ab7a09bf118f895ddf14a0193e4965fc0a07a
NazarRI <senior.ik@ya.ru>
2016-09-02 17:49:27 +0300

    Update Task2_3.java

--- src/Module2/Task2_3.java
+++ src/Module2/Task2_3.java
@@ -1,39 +1,39 @@
-package Module2;
 
 public class Task2_3 {
-    public static int[] balances = {1200, 100, 2000, 500, 3200};
-    static double withdrawBalance(String[] ownerName, double withdrawal) {
-        String ownN = ownerName[0];
-        double bal;
+    private static int[] balances = {1200, 300, 2000, 500, 3200};
+    private static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+    private static double comission = 0.05;
+
+    static double withdrawBalance(String ownerName, double withdrawal) {
+        String ownName = ownerNames[0];
+        int balance = balances[0];
         double wit = withdrawal;
-        double res;
-        res = wit * 0.05;
-        for (int i = 0; i < ownerName.length; i++) {
-            ownN = ownerName[3];
-        }
-        bal = balances[1];
-
-        bal = bal - res - wit;
-
-        System.out.print(ownN + " ");
-
-        return bal;
+        double com = comission * wit;
+        for (int i = 0; i < ownerNames.length; i++)
+            if (ownerNames[i] == ownerName)
+             System.out.println(ownerName + " " + (balances[i] - com - wit));
+                return com;
     }
 
+
+
+
     public static void main(String[] args) {
-
-        String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
-        int drawable = 80;
-
-        double res1 = withdrawBalance(ownerNames, drawable);
+        String ownerName = "Ann";
+        double withdrawal = 100;
+        double res1 = withdrawBalance(ownerName, withdrawal);
         if(res1 > 0) {
-            System.out.println(res1 +" "+drawable);
+            System.out.println(res1);
         }
         else{
             System.out.println("NO");
         }
 
-
-
-    }
-}
+        String ownerName2 = "Oww";
+        double withdrawal2 = 490;
+        double res2 = withdrawBalance(ownerName2, withdrawal2);
+        if(res2 > 0) {
+            System.out.println(res2);
+        }
+        else{
+            System.out.println("NO");
