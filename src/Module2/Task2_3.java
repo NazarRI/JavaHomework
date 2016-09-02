eaf1c1c32580986af21ced76d17dc7fb8a88318f
NazarRI <senior.ik@ya.ru>
2016-09-02 17:59:13 +0300

    Update Task2_3.java

--- src/Module2/Task2_3.java
+++ src/Module2/Task2_3.java
@@ -1,29 +1,30 @@
+package Module2;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class Task2_3 {
-    private static int[] balances = {1200, 300, 2000, 500, 3200};
+    private static int[] balances = {1200, 250, 2000, 500, 3200};
     private static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
     private static double comission = 0.05;
 
     static double withdrawBalance(String ownerName, double withdrawal) {
         String ownName = ownerNames[0];
-        int balance = balances[0];
+        double balance = balances[0];
         double wit = withdrawal;
         double com = comission * wit;
         for (int i = 0; i < ownerNames.length; i++)
-            if (ownerNames[i] == ownerName)
-             System.out.println(ownerName + " " + (balances[i] - com - wit));
-                return com;
+            if (ownerNames[i] == ownerName) balance = balances[i];
+        balance = balance - com - wit;
+                return balance;
     }
-
-
-
 
     public static void main(String[] args) {
         String ownerName = "Ann";
         double withdrawal = 100;
         double res1 = withdrawBalance(ownerName, withdrawal);
         if(res1 > 0) {
-            System.out.println(res1);
+            System.out.println(ownerName + " " + withdrawal + " " + res1);
         }
         else{
             System.out.println("NO");
@@ -33,7 +34,8 @@ public class Task2_3 {
         double withdrawal2 = 490;
         double res2 = withdrawBalance(ownerName2, withdrawal2);
         if(res2 > 0) {
-            System.out.println(res2);
+            System.out.println(ownerName2 + " " + withdrawal2 + " " + res2);
         }
         else{
-            System.out.println("NO");
+            System.out.println(ownerName2 + " NO");
+        }
